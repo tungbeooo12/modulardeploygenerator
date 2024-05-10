58119dd4cba2479ee489b36f4eb9c91ef40b728d
tungbeooo12 <babydog299pun0suy@hotmail.com>
2024-05-11 00:30:29 +0700

    update project commit

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-function moveZeroes(nums) {
-  let index = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== 0) {
-      nums[index++] = nums[i];
-    }
-  }
-  for (let i = index; i < nums.length; i++) {
-    nums[i] = 0;
-  }
-  return nums;
-}
+const titleCase = (str) =>
+  str.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
