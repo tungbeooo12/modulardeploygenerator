028c439b1cc85ccbacb76d7c8a2e2477e221f59c
tungbeooo12 <babydog299pun0suy@hotmail.com>
2024-05-14 05:01:13 +0700

    hi commit

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-function canPartition(nums) {
-  const sum = nums.reduce((acc, val) => acc + val, 0);
-  if (sum % 2 !== 0) return false;
-  const target = sum / 2;
-  const dp = new Array(target + 1).fill(false);
-  dp[0] = true;
-  for (const num of nums) {
-    for (let i = target; i >= num; i--) {
-      dp[i] = dp[i] || dp[i - num];
-    }
-  }
-  return dp[target];
-}
+const sum = (a, b) => a + b;
